feb89609685358a3a7473f9d054c94d36131ad4a
siddharth puri <96472864+Siddharthpuri99@users.noreply.github.com>
2024-12-11 22:27:31 +0530

    Update conn.js

--- backend/connetion/conn.js
+++ backend/connetion/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const con = async() => {
     try{
-       await  mongoose.connect(`${process.env.URI}`)
+       await  mongoose.connect(`${process.env.URI}`);
        console.log("Mongo connected successfully");
     }catch(err){
         console.log(err);
